cf57d1a4a552ddc2f935177a84f3508a6e6d3ea3
kimyk0101 <kimyk7131@gmail.com>
2024-12-02 13:09:02 +0900

    추상 인터페이스 연습 문제 1번 완료

--- src/com/javaex/ex01/Ractangle.java
+++ src/com/javaex/ex01/Ractangle.java
@@ -5,19 +5,40 @@ public class Ractangle extends Shape{
 	private int width;
 	private int height;
 	
+	
+	
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
 	public Ractangle(String fillColor, int width, int height){
 		super(fillColor);
 		this.width=width;
 		this.height=height;
 	}
 	
-	public void showInfo(){
+	@Override
+	public void draw(){
 		System.out.println("[사각형]#면색:" +super.fillColor+ " "
 				+ " #가로:" + width 
 				+" #세로:" + height + "그렸습니다.");                                                                        
 	}
-		
-
+	
+	public void width() {
+		System.out.println("sr1의 가로는 " + width + " 입니다");
+	}
 }
 
 
--- src/com/javaex/ex01/ShapeApp.java
+++ src/com/javaex/ex01/ShapeApp.java
@@ -4,15 +4,14 @@ public class ShapeApp {
 
 	public static void main(String[] args) {
 
-		Shape s = new Shape("빨강");
+//		Shape s = new Shape("빨강");
 		Circle c1 = new Circle("녹색", 10);
 		Ractangle r1 = new Ractangle("빨강", 4, 4);
 		Shape sr1 = new Ractangle("빨강", 4, 4);
 		
-		//sr1을 그리는 메소드 호출
 		
-		//sr1의 가로크기 출력
-		
+		((Ractangle) sr1).draw();
+		((Ractangle)sr1).width();
 	}
 }
 
